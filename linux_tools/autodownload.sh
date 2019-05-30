b2b31afbe2139ae774623df07b45e3b43d7a9b87
BarelyMiSSeD <weberje1@gmail.com>
2019-05-30 17:06:46 -0400

    Create autodownload.sh

---
+++ linux_tools/autodownload.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+me=`basename "$0"`
+echo "========== $me has started. =========="
+echo "========= $(date) ========="
+
+workshopIDs=`cat /home/steam/steamcmd/steamapps/common/qlds/baseq3/workshop.txt | grep -v '#' | sed '/^[ \t]*$/d'`
+numOfIDs=`echo "$workshopIDs" | wc -l`
+counter=0
+while [ $counter -lt $numOfIDs ]; do
+	currentID=`echo $workshopIDs | awk '{ print $1 }'`
+	workshopIDs=`echo $workshopIDs | cut -d ' ' -f2-`
+	echo -e "Downloading item $(expr $counter + 1) of $numOfIDs from Steam with id $currentID"
+	/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/steamcmd/steamapps/common/qlds/ +workshop_download_item 282440 $currentID +quit > /dev/null
+  if [ $? -ne 0 ]; then
+      echo -e "Download of id $currentID failed with steamcmd error code $?"
+  else
+      echo -e "Download of id $currentID completed"
+  fi
+	((counter++))
+done
+echo "Done."
+exit 0
