3846df35c7fefd9a2472440770b5cba78a46343e
BarelyMiSSeD <weberje1@gmail.com>
2019-07-04 09:57:45 -0400

    Add files via upload

--- linux_tools/autodownload.sh
+++ linux_tools/autodownload.sh
@@ -4,14 +4,18 @@ me=`basename "$0"`
 echo "========== $me has started. =========="
 echo "========= $(date) ========="
 
-workshopIDs=`cat /home/steam/steamcmd/steamapps/common/qlds/baseq3/workshop.txt | grep -v '#' | sed '/^[ \t]*$/d'`
+workshopFile='workshop.txt'
+steamCMD='/home/steam/steamcmd'
+installLocation='/home/steam/steamcmd/steamapps/common/qlds'
+workshopIDs=`cat $installLocation/baseq3/$workshopFile | grep -v '#' | sed '/^[ \t]*$/d'`
 numOfIDs=`echo "$workshopIDs" | wc -l`
 counter=0
+
 while [ $counter -lt $numOfIDs ]; do
 	currentID=`echo $workshopIDs | awk '{ print $1 }'`
 	workshopIDs=`echo $workshopIDs | cut -d ' ' -f2-`
 	echo -e "Downloading item $(expr $counter + 1) of $numOfIDs from Steam with id $currentID"
-	/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/steamcmd/steamapps/common/qlds/ +workshop_download_item 282440 $currentID +quit > /dev/null
+	$steamCMD/steamcmd.sh +login anonymous +force_install_dir /home/steam/steamcmd/steamapps/common/qlds/ +workshop_download_item 282440 $currentID +quit > /dev/null
   if [ $? -ne 0 ]; then
       echo -e "Download of id $currentID failed with steamcmd error code $?"
   else
